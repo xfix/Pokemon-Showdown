2ec0434cb427e4760b20447210b99c413e8aedce
Joimer <joimpoke@gmail.com>
2013-03-04 08:44:55 +0100

    Adding gen 1 ubers

--- data/formats.js
+++ data/formats.js
@@ -115,16 +115,27 @@ exports.BattleFormats = {
 	        'Keldeo', 'Keldeo-Resolute',  'Meloetta', 'Genesect'
 	    ]
     },
-	gen1dev: {
+	gen1oudev: {
 		mod: 'gen1',
 		effectType: 'Format',
-		name: "[Gen 1] Dev",
+		name: "[Gen 1] OU Dev",
 		challengeShow: true,
 		searchShow: true,
 		debug: true,
 		isTeambuilderFormat: true,
 		ruleset: ['Gen1 Pokemon', 'All Abilities', 'Sleep Clause', 'Species Clause', 'OHKO Clause', 'Evasion Clause'],
-		banlist: ['Uber', 'Illegal']
+		banlist: ['Uber', 'Illegal', 'Agility ++ Wrap']
+	},
+	gen1ubersdev: {
+		mod: 'gen1',
+		effectType: 'Format',
+		name: "[Gen 1] Ubers Dev",
+		challengeShow: true,
+		searchShow: true,
+		debug: true,
+		isTeambuilderFormat: true,
+		ruleset: ['Gen1 Pokemon', 'All Abilities', 'Sleep Clause', 'Species Clause'],
+		banlist: ['Illegal', 'Agility ++ Wrap']
 	},
 	/*gen1randombattle: {
 		mod: 'gen1',
@@ -1063,7 +1074,7 @@ exports.BattleFormats = {
 			// In gen 1, there's no advantage on having subpar EVs and you could max all of them
 			set.evs = {hp: 255, atk: 255, def: 255, spa: 255, spd: 255, spe: 255};
 				
-			// IVs worked different (DVs, 0 to 15) so we put all IVs to 30
+			// IVs worked different (DVs, 0 to 15, 2 points each) so we put all IVs to 30
 			set.ivs = {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30};
 			
 			// They also get a useless nature, since that didn't exist
