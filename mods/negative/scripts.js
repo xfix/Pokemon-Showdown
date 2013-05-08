e8454f303f8d8245916f2862afdb6c21cab169c7
Joimer <joimpoke@gmail.com>
2013-05-08 17:19:38 +0200

    Negative meta mod fix

--- mods/negative/scripts.js
+++ mods/negative/scripts.js
@@ -1,11 +1,18 @@
 exports.BattleScripts = {
 	init: function() {
 		for (var i in this.data.Pokedex) {
-			if (!i === 'shedinja') this.data.Pokedex[i].baseStats.hp = 160 - this.data.Pokedex[i].baseStats.hp;
-			this.data.Pokedex[i].baseStats.atk = 160 - this.data.Pokedex[i].baseStats.atk;
-			this.data.Pokedex[i].baseStats.def = 160 - this.data.Pokedex[i].baseStats.def;
-			this.data.Pokedex[i].baseStats.spa = 160 - this.data.Pokedex[i].baseStats.spa;
-			this.data.Pokedex[i].baseStats.spd = 160 - this.data.Pokedex[i].baseStats.spd;
+			var hp = 160 - this.data.Pokedex[i].baseStats.hp;
+			var atk = 160 - this.data.Pokedex[i].baseStats.atk;
+			var def = 160 - this.data.Pokedex[i].baseStats.def;
+			var spa = 160 - this.data.Pokedex[i].baseStats.spa;
+			var spd = 160 - this.data.Pokedex[i].baseStats.spd;
+			var spe = 160 - this.data.Pokedex[i].baseStats.spe;
+			this.data.Pokedex[i].baseStats.hp = (hp > 5)? (i === 'shedinja')? 1 : hp : 5;
+			this.data.Pokedex[i].baseStats.atk = (atk > 5)? atk : 5;
+			this.data.Pokedex[i].baseStats.def = (def > 5)? def : 5;
+			this.data.Pokedex[i].baseStats.spa = (spa > 5)? spa : 5;
+			this.data.Pokedex[i].baseStats.spd = (spd > 5)? spd : 5;
+			this.data.Pokedex[i].baseStats.spd = (spe > 5)? spe : 5;
 		}
 	}
 };
