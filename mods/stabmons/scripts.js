86485b873a39791dba0fc38eb7828af5d5c610c6
Joimer <joimpoke@gmail.com>
2013-07-02 06:50:25 +0200

    Add note command, add stabmons change

--- mods/stabmons/scripts.js
+++ mods/stabmons/scripts.js
@@ -1,12 +1,10 @@
 exports.BattleScripts = {
 	init: function() {
 		for (var i in exports.BattleLearnsets) {
-			var pokemon = exports.BattlePokedex[i];
+			console.log(i);
 			for (var n in exports.BattleMovedex) {
-				for (var t in pokemon.types) {
-					if (pokemon.types[t] === exports.BattleMovedex[n].type && !exports.BattleLearnsets[i].learnset[exports.BattleMovedex[n].id]) {
-						exports.BattleLearnsets[i].learnset[exports.BattleMovedex[n].id] = ["5L1"];
-					}
+				if ((exports.BattleMovedex[n].type in exports.BattlePokedex[i].types)) {
+					exports.BattleLearnsets[i].learnset[exports.BattleMovedex[n].id] = ["5L1"];
 				}
 			}
 			delete pokemon;
