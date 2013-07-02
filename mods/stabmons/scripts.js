b219861c54602269a0b8d316c288d8f1877db5c3
Joimer <joimpoke@gmail.com>
2013-07-02 08:06:10 +0200

    Stabmons: Ban sketch

--- mods/stabmons/scripts.js
+++ mods/stabmons/scripts.js
@@ -3,9 +3,11 @@ exports.BattleScripts = {
 		console.log('stabmons init');
 		for (var i in this.data.Learnsets) {
 			for (var n in this.data.Movedex) {
-				for (var t in this.data.Pokedex[i].types) {
-					if (this.data.Movedex[n].type === this.data.Pokedex[i].types[t]) {
-						this.data.Learnsets[i].learnset[this.data.Movedex[n].id] = ["5L1"];
+				if (this.data.Movedex[n].id !== 'sketch') {
+					for (var t in this.data.Pokedex[i].types) {
+						if (this.data.Movedex[n].type === this.data.Pokedex[i].types[t]) {
+							this.data.Learnsets[i].learnset[this.data.Movedex[n].id] = ["5L1"];
+						}
 					}
 				}
 			}
