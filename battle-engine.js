6d01fd2774fa6d9b167e769e49733c8b29e72476
Joim <joimpoke@gmail.com>
2013-07-26 14:47:35 +0200

    Add triples to battle-engine

--- battle-engine.js
+++ battle-engine.js
@@ -1095,6 +1095,9 @@ var BattleSide = (function() {
 		case 'doubles':
 			this.active = [null, null];
 			break;
+		case 'triples': case 'rotation': 
+			this.active = [null, null, null]; 
+			break;
 		}
 
 		this.team = this.battle.getTeam(this, team);
@@ -2637,7 +2640,7 @@ var Battle = (function() {
 		//int(int(int(2*L/5+2)*A*P/D)/50);
 		var baseDamage = Math.floor(Math.floor(Math.floor(2*level/5+2) * basePower * attack/defense)/50) + 2;
 
-		// multi-target modifier (doubles only)
+		// multi-target modifier (doubles and triples)
 		if (move.spreadHit) {
 			var spreadModifier = move.spreadModifier || 0.75;
 			this.debug('Spread modifier: ' + spreadModifier);
