a736b457a2454ed5abca677d22c3bbf3a02c38c0
Slayer95 <ivojulca@hotmail.com>
2014-02-21 14:25:58 -0500

    Update bans for Physical-Special swap

---
+++ mods/categoryswap/formats-data.js
@@ -0,0 +1,14 @@
+exports.BattleFormatsData = {
+	kyuremb: {
+		inherit: true,
+		tier: "Uber"
+	},
+	kyuremw: {
+		inherit: true,
+		tier: "OU"
+	},
+	reshiram: {
+		inherit: true,
+		tier: "OU"
+	}
+};
