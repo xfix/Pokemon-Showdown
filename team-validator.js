afeb22931c8c4606b79a9da75ca5941525c3d201
TalkTakesTime <b3nf@hotmail.com>
2014-03-05 22:59:08 +1300

    Slight edit of Alphabet Cup hack

--- team-validator.js
+++ team-validator.js
@@ -578,7 +578,7 @@ var Validator = (function() {
 			// Stabmons hack to avoid copying all of validateSet to formats.
 			if (format.id === 'stabmons' && template.types.indexOf(tools.getMove(move).type) > -1) return false;
 			// Alphabet Cup hack
-			if (format.id === 'alphabetcup' && template.species.slice(0,1).toLowerCase() === Tools.getMove(move).name.slice(0,1).toLowerCase() && Tools.getMove(move).name !== 'Sketch') return false;
+			if (format.id === 'alphabetcup' && template.speciesid.slice(0,1) === Tools.getMove(move).id.slice(0,1) && Tools.getMove(move).id !== 'sketch') return false;
 			if (template.learnset) {
 				if (template.learnset[move] || template.learnset['sketch']) {
 					sometimesPossible = true;
