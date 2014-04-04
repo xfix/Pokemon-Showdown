05a6b426c12fa6c4fd5a49d6b50fc42954ee3a12
TalkTakesTime <b3nf@hotmail.com>
2014-04-04 13:30:19 +1300

    Remove Ability Shift hack

--- mods/abilityshift/scripts.js
+++ mods/abilityshift/scripts.js
@@ -1,5 +1,10 @@
 exports.BattleScripts = {
 	init: function() {
+		for (var i in this.data.Learnsets) {
+			for (var m in this.data.Learnsets[i].learnset) {
+				this.modData('Learnsets', i).learnset[m] = ["6M"];
+			}
+		}
 		var self = this;
 		var abilityList = Object.keys(this.data.Abilities).sort().filter(function(i) {return !self.data.Abilities[i].isNonstandard});
 		for (var i in this.data.Pokedex) {
