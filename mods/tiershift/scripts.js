798790511ad42f7a366d5c07e61225666e5117d5
TalkTakesTime <b3nf@hotmail.com>
2014-05-30 13:38:39 +1200

    Add pre-alpha TS

---
+++ mods/tiershift/scripts.js
@@ -0,0 +1,30 @@
+exports.BattleScripts = {
+	init: function() {
+		for (var i in this.data.Pokedex) {
+			var tier = '';
+			var adjustment = 0;
+			if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
+			switch (tier) {
+			case 'BL':
+			case 'UU':
+				adjustment = 5;
+				break;
+			case 'BL2':
+			case 'RU':
+				adjustment = 10;
+				break;
+			case 'BL3':
+			case 'NU':
+			case 'NFE':
+			case 'LC':
+				adjustment = 15;
+			}
+
+			if (adjustment) {
+				for (var i in this.data.Pokedex[i].baseStats) {
+					this.modData('Pokedex', i).baseStats[i] += adjustment;
+				}
+			}
+		}
+	}
+};
