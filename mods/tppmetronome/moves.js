18e0351277dc9fe8d720a0fa7242dc500b10c90f
Konrad Borowski <x.fix@o2.pl>
2014-08-13 12:12:23 +0200

    OHKO hype.

--- mods/tppmetronome/moves.js
+++ mods/tppmetronome/moves.js
@@ -4,5 +4,33 @@ exports.BattleMovedex = {
 		onTryHit: function (target, pokemon) {
 			this.points(pokemon.side, 'AERIAL ACE Kreygasm', 10);
 		}
+	},
+	fissure: {
+		inherit: true,
+		onHit: function (target, pokemon) {
+			this.points(pokemon.side, 'OHKO', 10);
+			this.points(target.side, 'Hype', 10);
+		}
+	},
+	guillotine: {
+		inherit: true,
+		onHit: function (target, pokemon) {
+			this.points(pokemon.side, 'OHKO', 10);
+			this.points(target.side, 'Hype', 10);
+		}
+	},
+	horndrill: {
+		inherit: true,
+		onHit: function (target, pokemon) {
+			this.points(pokemon.side, 'OHKO', 10);
+			this.points(target.side, 'Hype', 10);
+		}
+	},
+	sheercold: {
+		inherit: true,
+		onHit: function (target, pokemon) {
+			this.points(pokemon.side, 'OHKO', 10);
+			this.points(target.side, 'Hype', 10);
+		}
 	}
 }
