a49eb2d450368dd085454c07609723509c2bed20
Konrad Borowski <x.fix@o2.pl>
2014-08-13 12:42:51 +0200

    Heal pulse hype!

--- mods/tppmetronome/moves.js
+++ mods/tppmetronome/moves.js
@@ -32,5 +32,15 @@ exports.BattleMovedex = {
 			this.points(pokemon.side, 'OHKO', 10);
 			this.points(target.side, 'Hype', 10);
 		}
+	},
+	healpulse: {
+		inherit: true,
+		onHit: function (target, source) {
+			if (target.hp !== target.maxhp) {
+				this.points(source.side, 'A good deed a day brings happiness to stay!', 20);
+			}
+			if (source.hasAbility('megalauncher')) this.heal(this.modify(target.maxhp, 0.75));
+			else this.heal(Math.ceil(target.maxhp * 0.5));
+		}
 	}
 }
