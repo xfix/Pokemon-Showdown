40ad4efa1d05d744d62ab8f3135b1373548c57f8
Konrad Borowski <x.fix@o2.pl>
2014-08-21 20:59:01 +0200

    Fix Venom Drench,

--- mods/tppmetronome/moves.js
+++ mods/tppmetronome/moves.js
@@ -323,8 +323,8 @@ exports.BattleMovedex = {
 		inherit: true,
 		onHit: function (target, source, move) {
 			if (target.status === 'psn' || target.status === 'tox') {
-				return this.boost({atk:-1, spa:-1, spe:-1}, target, source, move);
 				this.points(source.side, 'Rarely successful', 30);
+				return this.boost({atk:-1, spa:-1, spe:-1}, target, source, move);
 			}
 			return false;
 		}
