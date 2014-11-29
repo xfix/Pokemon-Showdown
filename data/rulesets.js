d4c753637bc86f654f25b7fa816195e31f9babc2
Joimer <joimpoke@gmail.com>
2014-11-30 00:00:06 +0100

    Implement Mega Rayquaza Ban for Ubers Mega Rayquaza is banned from Ubers. However, Rayquaza with the move Dragon Ascent is allowed. It is thus necessary to add a mod rule, Mega Rayquaza Ban Mod, which flags Rayquaza as not mega evolveable.

--- data/rulesets.js
+++ data/rulesets.js
@@ -696,8 +696,6 @@ exports.BattleFormats = {
 		effectType: 'Rule',
 		onStart: function () {
 			this.add('rule', 'Mega Rayquaza Ban Mod: You cannot mega evolve Rayquaza');
-		},
-		onStart: function () {
 			for (var i = 0; i < this.sides[0].pokemon.length; i++) {
 				if (this.sides[0].pokemon[i].speciesid === 'rayquaza') this.sides[0].pokemon[i].canMegaEvo = false;
 			}
