0ba865e6e33026828596e0de5c019c52a188920b
Guangcong Luo <guangcongluo@gmail.com>
2014-12-18 15:32:52 -0500

    Revert "Update Random Battle"
    Don't remove Balloon Rotom-Fan without asking, that's just rude.
    :|
    
    This reverts commit fcaee2500971df71c1dd687991bbd68070384ebb.

--- data/scripts.js
+++ data/scripts.js
@@ -1556,13 +1556,10 @@ exports.BattleScripts = {
 			Dusclops: 84, Porygon2: 82, Chansey: 78,
 
 			// Banned Mega
-			"Gengar-Mega": 68, "Kangaskhan-Mega": 72, "Lucario-Mega": 72, "Mawile-Mega": 72,
+			"Kangaskhan-Mega": 72, "Lucario-Mega": 72, "Mawile-Mega": 72,
 
 			// Holistic judgment
-			Articuno: 86, Genesect: 72, Sigilyph: 76, Xerneas: 66,
-
-			// ORAS
-			"Groudon-Primal": 70, "Kyogre-Primal": 70, "Rayquaza-Mega": 70
+			Articuno: 82, Genesect: 72, "Gengar-Mega": 68, "Rayquaza-Mega": 68, Sigilyph: 76, Xerneas: 66
 		};
 		var level = levelScale[template.tier] || 90;
 		if (customScale[template.name]) level = customScale[template.name];
