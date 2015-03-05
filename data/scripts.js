2cb48af5119256e710cdd325780dadecb5670fa4
cadaeic <roadrunner.x@outlook.com>
2015-03-05 12:16:57 -0500

    Staff Bros: Fix Zebraiken

--- data/scripts.js
+++ data/scripts.js
@@ -4801,10 +4801,10 @@ exports.BattleScripts = {
 				baseSignatureMove: 'wish', signatureMove: "Premonition",
 				evs: {hp:252, def:4, spd:252}, nature: 'Quiet'
 			},
-			'@zebraiken': {
+			'@Zebraiken': {
 				species: 'zebstrika', ability: 'Compound Eyes', item: 'Life Orb', gender: 'M',
 				moves: ['thunder', ['fire blast', 'focusblast', 'highjumpkick', 'meteormash'][this.random(3)], ['blizzard', 'iciclecrash', 'sleeppowder'][this.random(3)]], // why on earth does he learn Meteor Mash?
-				baseSignatureMove: 'protect', signatureMove: "bzzt",
+				baseSignatureMove: 'detect', signatureMove: "bzzt",
 				evs: {atk:4, spa:252, spe:252}, nature: 'Hasty' 
 			},
 			// Drivers.
