a334af518bc1e7f5577c43bc9eb0df01b98ce9c5
Konrad Borowski <x.fix@o2.pl>
2015-04-29 13:03:05 +0200

    Move Blindmons to other metagames.

--- config/formats.js
+++ config/formats.js
@@ -5132,6 +5132,41 @@ exports.Formats = [
 		ruleset: ['OU']
 	},
 	{
+		name: "Blindmons",
+		section: "Other Metagames",
+
+		searchShow: false,
+		mod: "blindmons",
+
+		onBeforeSwitchIn: function (pokemon) {
+			pokemon.illusion = pokemon;
+			pokemon.illusion.fullname = pokemon.side.id + ': ' + 'Pokémon ' + (1 + pokemon.position);
+		},
+
+		onSwitchIn: function (pokemon) {
+			if (pokemon.moves.length > 1) {
+				var showmoves = pokemon.side.name + '\'s Pokémon ' + (1 + pokemon.position) + ' knows ';
+
+				for (var i = 0; i < pokemon.moves.length - 1; i++) {
+					if (i > 0) showmoves += (i < pokemon.moves.length - 2 ? ', ' : ' and ');
+					showmoves += pokemon.moves[i];
+				}
+
+				this.add('-hint', showmoves + '.');
+			}
+		},
+
+		ruleset: ['Pokemon', 'Species Clause', 'Moody Clause', 'Baton Pass Clause', 'Evasion Moves Clause', 'OHKO Clause',
+			'Swagger Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Sleep Clause Mod', 'Cancel Mod'
+		],
+		banlist: ['Soul Dew', 'Gengarite', 'Kangaskhanite', 'Lucarionite', 'Mawilite', 'Salamencite',
+			'Arceus', 'Blaziken', 'Darkrai', 'Deoxys', 'Deoxys-Attack', 'Deoxys-Defense', 'Deoxys-Speed', 'Dialga',
+			'Genesect', 'Giratina', 'Giratina-Origin', 'Greninja', 'Groudon', 'Ho-Oh', 'Kyogre', 'Kyurem-White',
+			'Lugia', 'Mewtwo', 'Palkia', 'Rayquaza', 'Reshiram', 'Shaymin-Sky', 'Xerneas', 'Yveltal', 'Zekrom',
+			'Zoroark', 'Zorua'
+		]
+	},
+	{
 		name: "350 Cup",
 		section: "Other Metagames",
 
@@ -5444,41 +5479,6 @@ exports.Formats = [
 			}
 			return problems;
 		}
-	},
-	{
-		name: "Blindmons",
-		section: "Other Metagames",
-
-		searchShow: false,
-		mod: "blindmons",
-
-		onBeforeSwitchIn: function (pokemon) {
-			pokemon.illusion = pokemon;
-			pokemon.illusion.fullname = pokemon.side.id + ': ' + 'Pokémon ' + (1 + pokemon.position);
-		},
-		
-		onSwitchIn: function (pokemon) {			
-			if (pokemon.moves.length > 1) {
-				var showmoves = pokemon.side.name + '\'s Pokémon ' + (1 + pokemon.position) + ' knows ';
-
-				for (var i = 0; i < pokemon.moves.length - 1; i++) {
-					if (i > 0) showmoves += (i < pokemon.moves.length - 2 ? ', ' : ' and ');				
-					showmoves += pokemon.moves[i];				
-				}
-
-				this.add('-hint', showmoves + '.');
-			}
-		},
-
-		ruleset: ['Pokemon', 'Species Clause', 'Moody Clause', 'Baton Pass Clause', 'Evasion Moves Clause', 'OHKO Clause',
-			'Swagger Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Sleep Clause Mod', 'Cancel Mod'
-		],
-		banlist: ['Soul Dew', 'Gengarite', 'Kangaskhanite', 'Lucarionite', 'Mawilite', 'Salamencite',
-			'Arceus', 'Blaziken', 'Darkrai', 'Deoxys', 'Deoxys-Attack', 'Deoxys-Defense', 'Deoxys-Speed', 'Dialga',
-			'Genesect', 'Giratina', 'Giratina-Origin', 'Greninja', 'Groudon', 'Ho-Oh', 'Kyogre', 'Kyurem-White',
-			'Lugia', 'Mewtwo', 'Palkia', 'Rayquaza', 'Reshiram', 'Shaymin-Sky', 'Xerneas', 'Yveltal', 'Zekrom',
-			'Zoroark', 'Zorua'
-		]
 	},
 
 	// BW2 Singles
