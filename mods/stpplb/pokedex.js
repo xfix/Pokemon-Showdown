31716c06e47dbee2d38854969cf0a104c914d176
TieSoul <tdbaksteen@gmail.com>
2015-10-18 12:29:26 +0200

    Revert "Fixed Mega TieSoul"
    This reverts commit 9c25d0a8f80c113c8cf67d31d69f6f5d6576ec4f.

--- mods/stpplb/pokedex.js
+++ mods/stpplb/pokedex.js
@@ -1,4 +1,3 @@
 exports.BattlePokedex = {
-	rhyperior: {inherit: true, otherFormes:['rhyperiormega']},
-	rhyperiormega: {num:464,species:"Rhyperior-Mega",baseSpecies:'Rhyperior',forme:'Mega',formeLetter:'M',types:["Ground","Rock"],baseStats:{hp:115,atk:170,def:170,spa:55,spd:85,spe:40},abilities:{0:"Rock Head"},heightm:2.4,weightkg:282.8,color:"Gray",prevo:"rhydon",evoLevel:42,eggGroups:["Monster","Field"]},
+	rhyperiormega: {num:464,species:"Rhyperior-Mega",types:["Ground","Rock"],baseStats:{hp:115,atk:170,def:170,spa:55,spd:85,spe:40},abilities:{0:"Rock Head"},heightm:2.4,weightkg:282.8,color:"Gray",prevo:"rhydon",evoLevel:42,eggGroups:["Monster","Field"]},
 }
