d0e5600b5c6f30e2414da95bd1a19905b0d26d58
azum4roll <azum4roll@users.noreply.github.com>
2015-11-19 21:00:00 +0800

    Tweaked Ban Evade
    Now it doesn't give evasion between 50% and 75% HP but would raise evasion to 0 if it's lower than that at above 50% HP.

--- mods/stpplb/abilities.js
+++ mods/stpplb/abilities.js
@@ -387,16 +387,13 @@ exports.BattleAbilities = { // define custom abilities here.
 		onResidualOrder: 26,
 		onResidualSubOrder: 1,
 		onResidual: function (pokemon) {
-			if (pokemon.hp <= 3 * pokemon.maxhp / 4 && pokemon.hp > pokemon.maxhp / 2 && pokemon.boosts.evasion < 1) {
-				this.boost({evasion: 1 - pokemon.boosts.evasion}, pokemon);
-			}
-			if (pokemon.hp <= pokemon.maxhp / 2 && pokemon.hp > pokemon.maxhp / 4 && pokemon.boosts.evasion < 2) {
+			if (pokemon.hp > pokemon.maxhp / 2 && pokemon.boosts.evasion < 0) {
+				this.boost({evasion: 0 - pokemon.boosts.evasion}, pokemon);
+			} else if (pokemon.hp <= pokemon.maxhp / 2 && pokemon.hp > pokemon.maxhp / 4 && pokemon.boosts.evasion < 2) {
 				this.boost({evasion: 2 - pokemon.boosts.evasion}, pokemon);
-			}
-			if (pokemon.hp <= 1 * pokemon.maxhp / 4 && pokemon.hp > pokemon.maxhp / 32  && pokemon.boosts.evasion < 4) {
+			} else if (pokemon.hp <= pokemon.maxhp / 4 && pokemon.hp > pokemon.maxhp / 32 && pokemon.boosts.evasion < 4) {
 				this.boost({evasion: 4 - pokemon.boosts.evasion}, pokemon);
-			}
-			if (pokemon.hp <= pokemon.maxhp / 32 && pokemon.boosts.evasion < 6) {
+			} else if (pokemon.hp <= pokemon.maxhp / 32 && pokemon.boosts.evasion < 6) {
 				this.boost({evasion: 6 - pokemon.boosts.evasion}, pokemon);
 			}
 		},
