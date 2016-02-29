f50be93715a972bd850157aedef16661f7ad0ba2
Bär Halberkamp <fledaway@gmail.com>
2016-02-29 17:06:55 +0100

    Implement Crestfall's move

--- config/formats.js
+++ config/formats.js
@@ -1010,6 +1010,12 @@ exports.Formats = [
 					this.add('c|@bumbadadabum|There really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine\'s resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!');
 				};
 			}
+			if (move.id === 'lightofruin' && name === 'crestfall') {
+				move.name = 'Light of Unruin';
+				delete move.recoil;
+				move.drain = [1, 2];
+				move.self = {boosts: {def: -1, spd: -1}};
+			}
 			if (move.id === 'hyperbeam' && name === 'joim') {
 				move.name = 'Gaster Blaster';
 				move.type = 'Electric';
