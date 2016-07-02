b475decc41f96fc2deba6192e9a7e6d8f387569c
azum4roll <azum4roll@users.noreply.github.com>
2016-07-02 14:51:54 +0800

    Style and bug fixes

--- mods/classicstatswitch/scripts.js
+++ mods/classicstatswitch/scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.BattleScripts = {
-    init: function() {
+    init: function () {
         for (let i in this.data.Pokedex) {
             let template = this.getTemplate(i);
             let newStats = {};
@@ -9,10 +9,8 @@ exports.BattleScripts = {
             for (let j in template.baseStats) {
                 stats.push(template.baseStats[j]);
             }
-            let bestStatVal = Math.min.apply(Math, stats);
+            let bestStatVal = Math.max.apply(Math, stats);
             let worstStatVal = Math.min.apply(Math, stats);
-            let bestStat = [];
-            let worstStat = [];
             for (let j in template.baseStats) {
                 newStats[j] = template.baseStats[j];
                 if (template.baseStats[j] === bestStatVal) {
@@ -24,5 +22,5 @@ exports.BattleScripts = {
             }
             this.modData('Pokedex', i).baseStats = newStats;
         }
-    }
+    },
 };
