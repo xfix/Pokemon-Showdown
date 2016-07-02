bf83c49cb0a30e3629e3c5cfef882f7b261174c3
azum4roll <azum4roll@users.noreply.github.com>
2016-07-02 14:56:37 +0800

    more space -> tab

--- mods/crossevolution/scripts.js
+++ mods/crossevolution/scripts.js
@@ -1,5 +1,5 @@
 'use strict';
- 
+
 exports.BattleScripts = {
     pokemon: {
         formeChange: function (template) {
@@ -8,34 +8,34 @@ exports.BattleScripts = {
             if (!crossTemplate.exists) return false;
             let crossPrevoTemplate = this.battle.getTemplate(crossTemplate.prevo);
             if (!crossPrevoTemplate.exists) return false;
- 
+
             if (!template.abilities) return false;
             this.illusion = null;
             this.template = template;
- 
+
             this.types = template.types;
             this.addedType = '';
- 
+
             // Base Stat changes
             let delta = crossTemplate.baseStats['hp'] - crossPrevoTemplate.baseStats['hp'];
             let boostedHP = Math.floor(Math.floor(2 * (this.template.baseStats['hp'] + delta) + this.set.ivs['hp'] + Math.floor(this.set.evs['hp'] / 4) + 100) * this.level / 100 + 10);
             if (this.maxhp > 1 && this.maxhp < boostedHP) this.hp = this.maxhp = boostedHP;
- 
+
             for (let statName in this.stats) {
                 let stat = this.template.baseStats[statName];
                 stat += crossTemplate.baseStats[statName] - crossPrevoTemplate.baseStats[statName];
                 stat = Math.floor(Math.floor(2 * stat + this.set.ivs[statName] + Math.floor(this.set.evs[statName] / 4)) * this.level / 100 + 5);
- 
+
                 let nature = this.battle.getNature(this.set.nature);
                 if (statName === nature.plus) stat *= 1.1;
                 if (statName === nature.minus) stat *= 0.9;
                 this.baseStats[statName] = this.stats[statName] = Math.floor(stat);
             }
             this.speed = this.stats.spe;
- 
+
             // Type changes
             let newType = this.types;
-            if (this.types.length > 1)  {
+            if (this.types.length > 1) {
                 // Primary type
                 if (crossPrevoTemplate.types[0] !== crossTemplate.types[0]) {
                     newType[0] = crossTemplate.types[0];
@@ -57,7 +57,7 @@ exports.BattleScripts = {
                 }
             }
             if (newType !== this.types) this.types = newType;
- 
+
             this.crossEvolved = true;
             return true;
         },
