bf12204996480cb6252ef0440b13f7d449e530c0
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-08-30 19:26:32 +0000

    SSB Brawl: Framework

---
+++ mods/seasonal/random-teams.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const RandomTeams = require('../../data/random-teams');
+
+class RandomStaffBrosTeams extends RandomTeams {
+	randomTeam: function () {
+		let team = [];
+		let variant = (this.random(2) === 1);
+		let sets = {
+			
+		};
+		
+		// Generate the team randomly.
+		let pool = Object.keys(sets);
+		for (let i = 0; i < 6; i++) {
+			let name = this.sampleNoReplace(pool);
+			let set = sets[name];
+			set.level = 100;
+			set.name = name;
+			if (!set.ivs) {
+				set.ivs = {hp:31, atk:31, def:31, spa:31, spd:31, spe:31};
+			} else {
+				for (let iv in {hp:31, atk:31, def:31, spa:31, spd:31, spe:31}) {
+					set.ivs[iv] = iv in set.ivs ? set.ivs[iv] : 31;
+				}
+			}
+			// Assuming the hardcoded set evs are all legal.
+			if (!set.evs) set.evs = {hp:84, atk:84, def:84, spa:84, spd:84, spe:84};
+			set.moves = [this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves)].concat(set.signatureMove);
+			team.push(set);
+		}
+
+		return team;
+	}	
+}
